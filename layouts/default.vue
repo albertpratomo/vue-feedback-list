3763abe07085944671ae82e966b71e2c02343cd8
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-18 00:42:02 +0200

    feat: create default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,21 @@
 <template>
-    <main class="px-10 py-20 text-center">
-        <slot />
-        <Footer />
-        <div class="mx-auto mt-5 text-center text-sm opacity-25">
-            [Default Layout]
-        </div>
-    </main>
+    <div class="bg-slate-50 min-h-screen grid grid-rows-[auto_1fr]">
+        <header class="border-b">
+            <div class="container mx-auto py-4 px-8 flex justify-between items-center">
+                <img src="/logo.webp" height="70" width="70" class="mix-blend-multiply">
+
+                <div class="header-right"></div>
+            </div>
+        </header>
+    
+        <main class="container mx-auto">
+            <slot></slot>
+        </main>
+    </div>
 </template>
+
+<style>
+:root {
+    --un-default-border-color: #E2E8F0
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,15 @@
 </script>
 
 <template>
-    <div>
-        Hello
+    <Teleport to=".header-right">
+        hi
+    </Teleport>
+        
+    <div class="h-full grid grid-cols-12">
+        <FeedbackList class="col-span-4" />
+
+        <div class="col-span-8 py-20 px-20 bg-white">
+            <FeedbackViewer />
+        </div>
     </div>
 </template>
