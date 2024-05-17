37b59ff61dcba4a5c469211ac22bacdd75cfc84f
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-17 23:34:33 +0200

    chore: remove unneeded code

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Vue Feedback List'
+export const appDescription = 'Vue Feedback List'
