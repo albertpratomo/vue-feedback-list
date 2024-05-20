819a44d3ad8aaa6114293a9882846c17ccf859a5
Albert Pratomo <albertpratomo21@gmail.com>
2024-05-21 01:12:37 +0200

    feat: enable client side form validation

--- components/FeedbackCreateForm.vue
+++ components/FeedbackCreateForm.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts">
 import { useForm } from 'vee-validate'
 import type { IFetchError } from 'ofetch'
+import { LoaderCircle } from 'lucide-vue-next'
+import { toTypedSchema } from '@vee-validate/zod'
 
 const emit = defineEmits<{
     created: [feedback: StoredFeedback]
 }>()
 
 const form = useForm<Feedback>({
+    validationSchema: toTypedSchema(FeedbackSchema),
     initialValues: { type: 'Bug' },
 })
 
-async function onSubmit() {
+const onSubmit = form.handleSubmit(async (body) => {
     const feedback = await $fetch<StoredFeedback>('/api/feedback', {
         method: 'post',
-        body: form.values,
+        body,
     }).catch((e: IFetchError) => {
         if (e.statusCode === 422)
             form.setErrors(e.data.data.fieldErrors)
     })
 
-    emit('created', feedback!)
-}
+    if (feedback)
+        emit('created', feedback)
+})
 </script>
 
 <template>
@@ -94,6 +98,11 @@ async function onSubmit() {
             </Button>
 
             <Button type="submit">
+                <LoaderCircle
+                    v-if="form.isSubmitting.value"
+                    class="mr-2 size-4 animate-spin"
+                />
+
                 Send feedback
             </Button>
         </div>
